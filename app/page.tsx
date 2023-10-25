104cc67636ec3695a1870b7208f14b440dc5ff82
ouckah <aidan.ouckama@gmail.com>
2023-10-24 22:32:14 -0400

    ✨ Switch Feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,49 @@
+'use client'
+import { useState } from "react"
+
 export default function Home() {
+
+  const [ouckah, setOuckah] = useState(true);
+  const selected = `text-${ouckah ? "white" : "black"} text-2xl font-bold select-none`
+  const unselected = selected + " opacity-50";
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <>
+      <main className={`flex h-screen flex-col items-center justify-between bg-${ouckah ? "black" : "white"} p-24`}>
+        <div className="flex flex-row justify-center items-center w-full h-screen gap-5">
+          <h1 className={`text-${ouckah ? "white" : "black"} text-2xl font-bold uppercase select-none`}>I am</h1>
+          <div className="flex flex-col justify-center items-start">
+            <h1 className="text-black select-none">{ouckah ? (".") : ("")}</h1>
+
+            <button 
+              className={ouckah ? (selected) : (unselected)}
+              onClick={() => setOuckah(true)}
+            >
+              ouckah
+            </button>
+
+            <button 
+              className={ouckah ? (unselected) : (selected)}
+              onClick={() => setOuckah(false)}
+            >
+              aidan ouckama
+            </button>
+
+            <h1 className="text-white select-none">{ouckah ? ("") : (".")}</h1>
+          </div>
+        </div>
+      </main>
+      {
+        ouckah ? (
+          <div>
+
+          </div>
+        ) : (
+          <div>
+            
+          </div>
+        )
+      }
+    </>
   )
 }
